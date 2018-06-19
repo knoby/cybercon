c9f8dcd964078455c8fca748d0423705e0de7b8c
knoby <maximilian.brinkmann@gmx.net>
2018-06-19 21:56:54 +0200

    Create Port Config

--- src/cm_com/mod.rs
+++ src/cm_com/mod.rs
@@ -1,7 +1,31 @@
+extern crate serial;
+
 mod msg_gen;
 
+use cm_com::serial::prelude::*;
 
+fn connect_cm(port: &String) ->  Result<(), serial::Error> {
 
-pub fn samplemsg() {
-	println!("{:?}", msg_gen::gen_msg(msg_gen::OpCodes::Alive,false));
+	//Try to open connection to serial port
+	let sp = serial::open(port);
+
+	let mut sp = match sp {
+		Ok(serial_port) => serial_port,
+		Err(e) => return Err(e),
+	};
+
+	//Connection opend successfull --> reconfigure port
+	sp.reconfigure(&|settings| {
+		settings.set_baud_rate(serial::Baud2400)?;
+		settings.set_char_size(serial::Bits8);
+		settings.set_parity(serial::ParityOdd);
+		settings.set_stop_bits(serial::Stop1);
+		settings.set_flow_control(serial::FlowNone);
+		Ok(())
+	})?;
+	//Set signal pins
+	sp.set_dtr(true)?;
+	sp.set_rts(false)?;
+
+	Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,5 @@ mod cm_com;
 
 
 fn main() {
-	cm_com::samplemsg();
+	
 }
