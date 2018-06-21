0109a2d5645473b1b58fb1737695e63cc08bdd8a
knoby <maximilian.brinkmann@gmx.net>
2018-06-21 20:14:06 +0200

    Added temp. code for Connection Test

--- src/cm_com/mod.rs
+++ src/cm_com/mod.rs
@@ -3,11 +3,16 @@ extern crate serial;
 mod msg_gen;
 
 use cm_com::serial::prelude::*;
+use std::io::prelude::*;
+use self::msg_gen::OpCodes;
 
-fn connect_cm(port: &String) ->  Result<(), serial::Error> {
+use std::{thread, time};
+
+
+pub fn connect(port: String) ->  Result<(), serial::Error> {
 
 	//Try to open connection to serial port
-	let sp = serial::open(port);
+	let sp = serial::open(&port);
 
 	let mut sp = match sp {
 		Ok(serial_port) => serial_port,
@@ -26,6 +31,33 @@ fn connect_cm(port: &String) ->  Result<(), serial::Error> {
 	//Set signal pins
 	sp.set_dtr(true)?;
 	sp.set_rts(false)?;
+
+
+	//Connection Test
+	let one_second = time::Duration::from_millis(1000);
+	let mut serial_buf: Vec<u8> = vec![0; 100];	
+
+	sp.write_all(&msg_gen::gen_msg(OpCodes::Alive, false))?;
+	thread::sleep(one_second);
+	match sp.read(serial_buf.as_mut_slice()) {
+        Ok(t) => println!("{:?}", t),
+        Err(e) => eprintln!("{:?}", e),
+    }
+	println!("{:?}", &serial_buf);
+	sp.write_all(&msg_gen::gen_msg(OpCodes::UnlockFirmware, false))?;
+	thread::sleep(one_second);
+	match sp.read(serial_buf.as_mut_slice()) {
+        Ok(t) => println!("{:?}", t),
+        Err(e) => eprintln!("{:?}", e),
+    }
+	println!("{:?}", &serial_buf);
+	sp.write_all(&msg_gen::gen_msg(OpCodes::PlaySound(0), false))?;
+	thread::sleep(one_second);
+	match sp.read(serial_buf.as_mut_slice()) {
+        Ok(t) => println!("{:?}", t),
+        Err(e) => eprintln!("{:?}", e),
+    }
+	println!("{:?}", &serial_buf);
 
 	Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,5 @@ mod cm_com;
 
 
 fn main() {
-	
+	cm_com::connect("/dev/ttyUSB0".to_string());
 }
